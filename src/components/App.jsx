39ebb5cb6dbfba7b7bd531c39508e74368b10c09
BogdanMU <104568558+BogdanMU@users.noreply.github.com>
2023-01-11 17:14:41 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,13 @@ import { getContacts } from 'redux/selectors';
 
 export const App = () => {
   const contacts = useSelector(getContacts);
-
   return (
     <Wrap>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
       <ContactsFilter />
-      {contacts !== [] ? <ContactsList /> : <p>Add some friends!</p>}
+      {contacts.length !== 0 ? <ContactsList /> : <div>Add some friends!</div>}
     </Wrap>
   );
 };
